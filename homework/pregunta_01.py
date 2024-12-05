f619e4fc8396639f1babdeac001f35921497c31c
sagonzalezbu <70988399+sagonzalezbu@users.noreply.github.com>
2024-12-04 20:28:29 -0500

    Intento completo

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -18,3 +18,39 @@ def pregunta_01():
 
 
     """
+    import pandas as pd
+
+    with open('files/input/clusters_report.txt', 'r', encoding='utf-8') as file:
+      lines = file.readlines()
+
+    # for i in range(6):
+    #     print(i, lines[i])
+
+    info = lines[4:]
+
+    data = [["cluster", "cantidad_de_palabras_clave", "porcentaje_de_palabras_clave", "principales_palabras_clave"]]
+
+    listAux = []
+    primera = True
+    for linea in info:
+        linea.strip()
+        linea = linea.split()
+        if len(linea) > 0 and primera:
+          listAux.append(int(linea[0]))
+          listAux.append(int(linea[1]))
+          listAux.append(float(linea[2].replace(',','.')))
+          listAux.append(" ".join(linea[4:]))
+          primera = False
+
+        elif len(linea) > 0:
+          listAux.append(" ".join(linea))
+
+        else:
+          primera = True
+          listAux[3] = ' '.join(listAux[3:]).replace('.','')
+          data.append(listAux[:4])
+          listAux = []
+
+    return pd.DataFrame(data[1:], columns= data[0])
+
+#print(pregunta_01())
